e64eb11cf0bea4712f83b1892b8d102a31ca7e4b
Kelvin Gikonyo <gikonyosenior@gmail.com>
2024-10-01 18:46:52 +0300

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 1;
+let num2 = 62
+ const multiply = num1 * num2;
 
-// Write your code here
+ const random = Math.ceil(Math.random());
+ 
+ let num3 = 6;
+ let num4 = 22;
+ const mod = num4 % num3;
+
+ const max = Math.max(2,5,8,20);
